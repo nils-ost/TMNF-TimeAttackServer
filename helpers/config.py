e4f134c090d0c597be634793335ff57aae1d1f92
Nils Ost <home@nijos.de>
2022-02-15 23:04:38 +0100

    some db stuff

---
+++ helpers/config.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+
+config = {
+    'tmnf-server': {
+        'host': 'localhost',
+        'port': 5000,
+        'user': 'SuperAdmin',
+        'password': 'SuperAdmin'
+    },
+    'mongo': {
+        'host': 'localhost',
+        'port': 27017,
+        'database': 'tmnf-ta'
+    }
+}
+
+if os.path.isfile('config.json'):
+    with open('config.json', 'r') as f:
+        config.update(json.loads(f.read()))
+else:
+    with open('config.json', 'w') as f:
+        f.write(json.dumps(config, indent=4))
+
+
+def get_config(portion=None):
+    global config
+    if portion is None:
+        return config
+    else:
+        return config.get(portion, None)
---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task, call
+
+
+@task(name="dev-start")
+def start_development(c):
+    r = c.run("sudo docker ps -f name=dev-mongo", hide=True)
+    if 'dev-mongo' not in r.stdout:
+        print("Starting mongoDB")
+        c.run("sudo docker run --name dev-mongo --rm -v /media/ramdisk/mongodb:/data/db -p 27017:27017 -d mongo:4.4")
+    c.run("python install/wait_for_mongodb.py")
+
+
+@task(name="dev-stop")
+def stop_development(c):
+    for name in ['dev-mongo']:
+        r = c.run(f"sudo docker ps -f name={name}", hide=True)
+        if name in r.stdout:
+            print(f"Stopping {name}")
+            c.run(f"sudo docker stop {name}")
+    print('Removing storage for dev-mongo')
+    c.run('sudo rm -rf /media/ramdisk/mongodb')
