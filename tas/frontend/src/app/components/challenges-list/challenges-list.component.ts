582e0e7a13d1f35411ce065d16f197ec7da788e8
Nils Ost <home@nijos.de>
2022-03-15 09:50:49 +0100

    more infos in challenges-list

--- tas/frontend/src/app/components/challenges-list/challenges-list.component.ts
+++ tas/frontend/src/app/components/challenges-list/challenges-list.component.ts
@@ -1,18 +1,81 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Challenge } from '../../interfaces/challenge';
+
+interface SortableChallenge {
+  id: string;
+  name: string;
+  seen_count: number;
+  seen_last: number;
+  time_limit: number;
+  up_in: number;
+  up_at: number;
+}
 
 @Component({
   selector: 'app-challenges-list',
   templateUrl: './challenges-list.component.html',
   styleUrls: ['./challenges-list.component.scss']
 })
-export class ChallengesListComponent implements OnInit {
+export class ChallengesListComponent implements OnInit, OnChanges {
   @Input() challenges!: Challenge[];
   @Input() currentChallenge!: Challenge;
+  sortableChallenges: SortableChallenge[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.buildSortableChallenges();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.buildSortableChallenges();
+  }
+
+  buildSortableChallenges() {
+    if (this.currentChallenge) {
+      let scs: SortableChallenge[] = [];
+      let c_index = -1;
+      let up_in = 0;
+      let up_at = 0;
+      for (let i = 0; i < this.challenges.length; i++) {
+        let c: Challenge = this.challenges[i];
+        if (c_index > -1) {
+          up_in += Math.floor(c.time_limit / 1000);
+          up_at += Math.floor(c.time_limit / 1000);
+          let sc: SortableChallenge = {
+            id: c.id,
+            name: c.name,
+            seen_count: c.seen_count,
+            seen_last: c.seen_last,
+            time_limit: c.time_limit,
+            up_in: Math.floor(up_in / 60),
+            up_at: up_at
+          } as SortableChallenge;
+          scs.push(sc);
+        }
+        if (c.id === this.currentChallenge.id) {
+          c_index = i;
+          up_in = c.seen_last - (Math.floor(Date.now()/1000));
+          up_at = c.seen_last;
+        }
+      }
+      for (let i = 0; i <= c_index; i++) {
+        let c: Challenge = this.challenges[i];
+        up_in += Math.floor(c.time_limit / 1000);
+        up_at += Math.floor(c.time_limit / 1000);
+        let sc: SortableChallenge = {
+          id: c.id,
+          name: c.name,
+          seen_count: c.seen_count,
+          seen_last: c.seen_last,
+          time_limit: c.time_limit,
+          up_in: Math.floor(up_in / 60),
+          up_at: up_at
+        } as SortableChallenge;
+        scs.push(sc);
+      }
+      this.sortableChallenges = scs;
+    }
   }
 
 }
